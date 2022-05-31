c98ea48ed2ac94fbf2001c5f869901473dd31ed0
Timofei <banantvori6505@gmail.com>
2022-05-31 21:51:51 +0300

    nrthrth

--- script.js
+++ script.js
@@ -8,11 +8,35 @@ cnopki1.addEventListener('click', function(event){
     let space = " "
     
     let statusOperator
-    let nowLustitemIndex
+    let nowLastItemIndex
+
+    nowLastItemIndex = display.value.length - 1
+    statusOperator = checkLastItem(nowLastItemIndex)
+
+
     console.log(event.target.closest("button"))
     
     if (event.target.closest("button") != null) {
     
+        if (statusOperator) {
+            let nowStr = display.value 
+            nowStr = nowStr.split("")
+            nowStr[nowLastItemIndex] = "*"
+            display.value = nowStr.join("")
+        } else {
+            display.value += "*"
+        }
+
+        Result.addEventListener("mouseover", function () {
+            Result.style.color = "red"
+            Result.style.background = "pink"
+        
+        })
+        Result.addEventListener("mouseout", function () {
+            Result.style.color = "black"
+            Result.style.background = "rgb(239, 239, 239)"
+        })
+        
     
     switch(value){
     case 'AC':
