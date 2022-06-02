28ddd3595ec68f6b48620120433549b4bc1daa82
Timofei <banantvori6505@gmail.com>
2022-06-02 10:35:38 +0300

    ergergergegr

--- script.js
+++ script.js
@@ -6,36 +6,10 @@ cnopki1.addEventListener('click', function(event){
     let value = event.target.innerText
     
     let space = " "
-    
-    let statusOperator
-    let nowLastItemIndex
-
-    nowLastItemIndex = display.value.length - 1
-    statusOperator = checkLastItem(nowLastItemIndex)
-
 
     console.log(event.target.closest("button"))
     
     if (event.target.closest("button") != null) {
-    
-        if (statusOperator) {
-            let nowStr = display.value 
-            nowStr = nowStr.split("")
-            nowStr[nowLastItemIndex] = "*"
-            display.value = nowStr.join("")
-        } else {
-            display.value += "*"
-        }
-
-        Result.addEventListener("mouseover", function () {
-            Result.style.color = "red"
-            Result.style.background = "pink"
-        
-        })
-        Result.addEventListener("mouseout", function () {
-            Result.style.color = "black"
-            Result.style.background = "rgb(239, 239, 239)"
-        })
         
     
     switch(value){
